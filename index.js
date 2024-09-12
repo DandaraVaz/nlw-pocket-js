159c6fa88dabc6c81b4e6cf33cd6460f5194729e
Dandara Vaz <dandarapatriciovaz@gmail.com>
2024-09-12 16:09:38 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -53,10 +53,26 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
     })
 }
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select({
+        message: "Metas abertas " + abertas.length,
+        choices: [...abertas]
+    }) 
+} 
 
 const start = async() => {
 
@@ -71,6 +87,10 @@ const start = async() => {
                 {
                     name: "Listar metas",
                     value: "listar"
+                },
+                {
+                    name: "Metas abertas",
+                    value: "abertas"
                 },
                 {
                     name: "Metas realizadas",
@@ -93,6 +113,9 @@ const start = async() => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!")
